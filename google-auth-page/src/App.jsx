e794d4de2eb409abed8eeebccca49bec5b2b2dba
SumitChatterjeee <subrothochatt123@gmail.com>
2026-08-09 21:09:53 +0530

    Thanks page is Added

--- google-auth-page/src/App.jsx
+++ google-auth-page/src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
-import Login from './pages/Login'
 import Thanks from './pages/Thanks'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/thanks" element={<Thanks />} />
+      <Route path="/" element={<Thanks/>} />
     </Routes>
   )
 }
